b14f833196fe653b2b61e04b9e3a8bd4ca175ffb
zfrehner <59748596+zfrehner@users.noreply.github.com>
2020-11-25 14:45:44 -0800

    Add files via upload

--- lib/home/gyms.dart
+++ lib/home/gyms.dart
@@ -5,30 +5,118 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:spotlight_login/constants.dart';
+import 'package:spotlight_login/gymViews/gymCardView.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Gyms extends StatefulWidget {
   @override
   _GymsState createState() => _GymsState();
 }
-
 class _GymsState extends State<Gyms> {
+
+  //set of markers to mark the gyms
+  Set<Marker> _markers = Set<Marker>();
+  BitmapDescriptor _markerIcon;
+  GoogleMapController _mapController;
+
+  void initState() {
+    //getCurrentUser();
+    //_setMarkerIcon();
+  }
+
+  //used to set the icon for the map icons - getting from an asset image
+  //void _setMarkerIcon() async {
+    //_markerIcon = await BitmapDescriptor.fromAssetImage(ImageConfiguration(), "assets/images/.jpg");
+  //}
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+
+    setState(() {
+      _markers.add(Marker(
+          markerId: MarkerId("1"),
+          position: LatLng(43.847000, -79.551850),
+          infoWindow: InfoWindow(title: "Good Life Fitness #1",
+              onTap: () {print("icon 1 tapped");},
+          snippet: "3420 Major MacKenzie Dr W #201 Vaughan, ON L4H 4J6"),
+          )); //icon: _markerIcon-- add this to add an image for marker icons
+
+      _markers.add(Marker(
+          markerId: MarkerId("2"),
+          position: LatLng(43.790910, -79.544790),
+          infoWindow: InfoWindow(title: "Good Life Fitness #2",
+              onTap: () {print("icon 2 tapped");},
+              snippet: "57 Northview Blvd Woodbridge, ON L4L 8X9")));
+
+      _markers.add(Marker(
+          markerId: MarkerId("3"),
+          position: LatLng(43.790050, -79.529910),
+          infoWindow: InfoWindow(title: "Good Life Fitness #3",
+              onTap: () {print("icon 3 tapped");},
+              snippet: "90 Interchange Way Concord, ON L4K 5Z8")));
+
+      _markers.add(Marker(
+          markerId: MarkerId("4"),
+          position: LatLng(43.798580, -79.501129),
+          infoWindow: InfoWindow(title: "Good Life Fitness #4",
+              onTap: () {print("icon 4 tapped");},
+              snippet: "7700 Keele St Concord, ON L4K 2A1")));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: <Widget>[ GoogleMap(
+          onMapCreated: _onMapCreated,
+          markers: _markers,
+          //myLocationEnabled: true,
+          initialCameraPosition: CameraPosition(
+              target: LatLng(43.7983, -79.5079),//this is the lat/lng for concord, CANADA
+              zoom: 10
+          ),
+        ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.only(bottom: 30),
+            child: FloatingActionButton(
+              onPressed: () {
+                //navigate to the list view of gyms
+                Navigator.pushNamed(context, GymCardView.id);
+              },
+              child: Icon(Icons.format_list_numbered_rounded),
+              backgroundColor: Colors.red,
+              foregroundColor: Colors.white,
+            )
+          )
+      ]),
+    );
+  }
+}
+
+
+/*class _GymsState extends State<Gyms> {
   final _auth = FirebaseAuth.instance;
   var firebaseUser = FirebaseAuth.instance.currentUser;
   FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   User loggedInUser;
 
+
   void initState() {
     getCurrentUser();
   }
 
-  /*void gymStream() async {
+  */
+/*void gymStream() async {
     await for(var snapshot in _firestore.collection("Gyms").snapshots()) {
       for(var info in snapshot.docs) {
         print(info.data());
       }
     }
   }*/
+/*
 
   void getCurrentUser() async {
     try {
@@ -95,9 +183,9 @@ class _GymsState extends State<Gyms> {
       ),
     ]);
   }
-}
+}*/
 
-class GymDisplay extends StatelessWidget {
+/*class GymDisplay extends StatelessWidget {
   GymDisplay({this.title, this.address, this.city, this.numUsers}); //, this.numUsers
 
   final String title;
@@ -173,7 +261,7 @@ class GymDisplay extends StatelessWidget {
       ),
     );
   }
-}
+}*/
 
 /*class GymDisplay extends StatelessWidget {
 
@@ -273,3 +361,5 @@ children: gymWidgets,
 
 }
 }*/
+
+
