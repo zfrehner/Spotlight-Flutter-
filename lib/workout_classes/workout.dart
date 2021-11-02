00d1cb158961a9dbd55456cae17aa5a824e42321
Phillip Ball <pokemon19977@hotmail.com>
2021-11-02 13:24:24 -0700

    Removed bottom-navbar, moved body from workouts to workout.

--- lib/workout_classes/workout.dart
+++ lib/workout_classes/workout.dart
@@ -3,11 +3,6 @@ import 'package:date_format/date_format.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'workouts.dart';
-import 'package:spotlight_login/home/calendar.dart';
-import 'package:spotlight_login/home/gyms.dart';
-import 'package:spotlight_login/home/messenger.dart';
-import 'package:spotlight_login/home/profile.dart';
-
 
 
 class Workout extends StatefulWidget {
@@ -20,35 +15,19 @@ class Workout extends StatefulWidget {
 class _WorkoutState extends State<Workout> {
   @override
 
-  final List<Widget> tabs = [
-    /*Center(
-      child: Text('Profile'),
-    ),
-    Center(
-      child: Text('Messages'),
-    ),
-    Column(
-        children: <Widget>[ Text("Gyms"),
-        ]),
-    Center(
-      child: Text('Calendar'),
-    ),*/
-    Profile(),
-    Calendar(),
-    Gyms(),
-    Messenger(),
-    Workouts()
-  ];
+  Map<String, bool> workouts = {
+    'Biceps':false,
+    'Shoulders':false,
+    'Triceps':false,
+    'Chest':false,
+    'Back':false,
+    'Legs':false,
 
-  int _currentIndex = 4;
-  
-  
+  };
 
   Widget build(BuildContext context) {
-    // var today = _currentDate;
     var args = ModalRoute.of(context).settings.arguments;
     var today = formatDate(DateTime.parse(args.toString().substring(0,10)),[MM,' - ',dd]);
-
 
     return Scaffold(
       appBar: AppBar(
@@ -72,66 +51,29 @@ class _WorkoutState extends State<Workout> {
             },
           ),
         ],
-      ),//***********************************************************
-      body: /*Column(
-        children: <Widget>[
-          Container(
-              child: Text(
-                  "Welcome " + loggedInUser.email
-              )
-          ),
-          Container(
-            child: InkWell(
-                child: Text("It's time to be someone at the Gym!",
-                    style: kLoginTextStyle,
-                ),
-                onTap: () => launch('https://nullpointerexception.greenriverdev.com/Spotlight/index.php')
-            ),
-          ),
-          Image(
-            image: AssetImage('assets/images/gym5.jpg'),
-          ),
-          tabs[_currentIndex]
-        ],
-      ),*/
-      tabs[_currentIndex],
-
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        iconSize: 0.0,
-        unselectedFontSize: 14.0,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.white,
-        //currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            backgroundColor: Colors.white,
-            icon: Icon(Icons.contacts_rounded),
-            label: 'Profile',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today_rounded),
-            label: 'Calendar',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fitness_center),
-            label: 'Gyms',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu_book_rounded),
-            label: 'Education',
-          ),
-        ],
-        onTap: (index){
-          setState(() {
-           _currentIndex = index;
-
-          }
-          );
-        },
       ),
+
+        body:
+          ListTileTheme(
+              textColor: Colors.black,
+              tileColor: Colors.redAccent,
+
+              child: ListView(
+                children: workouts.keys.map((String key) {
+                  return new CheckboxListTile(
+                    checkColor: Colors.black,
+                    contentPadding: EdgeInsets.fromLTRB(30, 0, 250, 0),
+                    title: new Text(key),
+                    value: workouts[key],
+                    onChanged: (bool value){
+                      setState(() {
+                        workouts[key] = value;
+                      });
+                    },
+                  );
+                }).toList(),
+              )
+          )
     );
   }
 }
